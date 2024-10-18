f9a87938a6da854e7edc07b3eb17752f1bf33a6d
lunamm25 <159203591+lunamm25@users.noreply.github.com>
2024-10-17 23:30:18 -0500

    Add files via upload

---
+++ my_first_api/src/test/java/com/my_first_api/my_first_api/MyFirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_first_api.my_first_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
